79778a03b86adc2aa1ee2685bcfca783711c1fc3
郭小凡 <glider.guo@ankobot.com>
2021-12-13 18:47:30 +0800

    Revert "test5"
    This reverts commit 94a526ec3197c739cef85b6827cf57f8b420eced.

--- vv.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: your name
- * @Date: 2021-12-13 18:39:47
- * @LastEditTime: 2021-12-13 18:39:48
- * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: /test_git/vv.cpp
- */
