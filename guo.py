ef71e71b3209295a93d8c84f694eade1054575aa
郭小凡 <glider.guo@ankobot.com>
2021-12-13 18:19:24 +0800

    test3

---
+++ guo.py
@@ -0,0 +1,9 @@
+'''
+Author: your name
+Date: 2021-12-13 18:18:58
+LastEditTime: 2021-12-13 18:18:59
+LastEditors: Please set LastEditors
+Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+FilePath: /test_git/guo.py
+'''
+a=1
