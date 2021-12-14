e5227ce5ceaba3b6def95233bb2e00bcfd9aea1e
glider110 <58673620+glider110@users.noreply.github.com>
2021-12-14 09:34:45 +0800

    a=66

--- guo.py
+++ guo.py
@@ -6,4 +6,4 @@ LastEditors: Please set LastEditors
 Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 FilePath: /test_git/guo.py
 '''
-a=3
+a=66
